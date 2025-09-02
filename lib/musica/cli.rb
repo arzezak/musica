3d28bca55ac875d9988f4f9ad156d647e7eb73d5
Ariel Rzezak <arzezak@gmail.com>
2025-09-01 23:18:59 -0300

    Allow to config the player

--- lib/musica/cli.rb
+++ lib/musica/cli.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+require "yaml"
+require "fileutils"
+
 module Musica
   class Cli
     def initialize(argv)
       @prompt = argv.join(" ")
-      @player = Musica::Player.new(application: "Music")
+      @player = Musica::Player.new(application: load_config)
     end
 
     def run
@@ -15,5 +18,24 @@ module Musica
     private
 
     attr_reader :argv, :player, :prompt
+
+    def load_config
+      config_dir = "#{ENV["XDG_CONFIG_HOME"]}/musica" || File.expand_path("~/.config/musica")
+      config_file = File.join(config_dir, "config.yml")
+
+      unless File.exist?(config_file)
+        create_config(config_dir, config_file)
+      end
+
+      YAML.load_file(config_file).dig("player")
+    end
+
+    def create_config(config_dir, config_file)
+      FileUtils.mkdir_p(config_dir)
+
+      default_config = { "player" => "Music" }
+
+      File.write(config_file, YAML.dump(default_config))
+    end
   end
 end
